8c35f9804d79f5217eb8c4b4b9b9381fb5f024b7
drkiettran <ktran@harrisburu.edu>
2020-02-03 05:13:07 -0500

    Initial checkin

---
+++ src/test/java/com/drkiettran/springmongodb/SpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drkiettran.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
